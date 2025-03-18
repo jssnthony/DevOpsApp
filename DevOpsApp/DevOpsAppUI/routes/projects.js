4aef2bbae47288fe8764fce81d430125165b7c53
JssNThony <jssnthony@gmail.com>
2025-03-18 01:00:28 -0700

    Added main layout

--- DevOpsApp/DevOpsAppUI/routes/projects.js
+++ DevOpsApp/DevOpsAppUI/routes/projects.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET projects listing. */
 router.get('/', function (req, res) {
     res.render('projects', { title: 'Projects' });
 });
