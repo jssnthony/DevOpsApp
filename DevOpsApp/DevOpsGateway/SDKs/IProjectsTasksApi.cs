a34909d9656910cdef2a07b90e6b69a2812446a5
JssNThony <jssnthony@gmail.com>
2025-04-06 21:58:30 -0700

    Added endpoints to gate way

---
+++ DevOpsApp/DevOpsGateway/SDKs/IProjectsTasksApi.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.Projects;
+using Models.ProjectsTasks;
+using Refit;
+
+namespace DevOpsGateway.SDKs
+{
+    public interface IProjectsTasksApi
+    {
+        [Get("/ProjectsTasks/{taskId}")]
+        Task<ProjectsTasksResultDto> GetProjectsTaskAsync(Guid taskId);
+
+        [Get("/ProjectsTasks/GetAll/{projectId}")]
+        Task<IEnumerable<ProjectsTasksResultDto>> GetAllProjectsTasksAsync(Guid projectId);
+
+        [Post("/ProjectsTasks/{projectId}")]
+        Task<ProjectsTasksResultDto> InsertProjectTaskAsync(Guid projectId, ProjectTaskToInsert toInsert);
+
+        [Put("/ProjectsTasks/Update/{taskId}")]
+        Task<ProjectsTasksResultDto?> UpdateProjectTaskAsync(Guid taskId, ProjectTaskToUpdate toUpdate);
+
+        [Put("/ProjectsTasks/AlterStatus/{taskId}")]
+        Task<bool> AlterProjectTaskStatusAsync(Guid taskId);
+
+    }
+}
